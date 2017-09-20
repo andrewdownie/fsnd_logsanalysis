8ec211e20c5419dcf2038dfa976178e8e2127dbe
Andrew Downie <andrew_dev@outlook.com>
2017-09-20 16:12:09 -0400

    Updated project, following the feedback from udacity.

--- vagrant/databaseAnalysis.py
+++ vagrant/databaseAnalysis.py
@@ -2,10 +2,11 @@
 
 import psycopg2
 
+
 # Question 1 and 2 require view: article_views
 """
 CREATE VIEW article_views AS
-SELECT COUNT(REPLACE(path, '/article/', '')),
+SELECT COUNT(path),
        articles.slug,
        articles.title,
        articles.author
@@ -17,10 +18,12 @@ SELECT COUNT(REPLACE(path, '/article/', '')),
     ORDER BY count DESC;
 """
 
+
 # Question 1: the top 3 articles
 QUESTION1 = """
-SELECT * FROM article_views ORDER BY count DESC {limit};
+SELECT * FROM article_views ORDER BY count DESC LIMIT %(limit)s;
 """
+
 
 # Question 2: authors ranked by article views
 QUESTION2 = """
@@ -33,13 +36,12 @@ SELECT SUM(named_authors.count), named_authors.name FROM
 AS named_authors
 GROUP BY named_authors.name
 ORDER BY sum DESC;
-
 """
 
 
 # Question 3 requires view: dailyStatus
 """
-CREATE VIEW dailyStatus AS
+CREATE VIEW daily_status AS
 SELECT COUNT(status),
           status,
           LEFT(CAST(time AS TEXT), 10)
@@ -48,69 +50,79 @@ SELECT COUNT(status),
    GROUP BY day, status;
 """
 
+
 # Question 3: days with error rates greater than 1%
 QUESTION3 = """
 SELECT * FROM
-    (SELECT totals.day, (errors.count / totals.sum) AS percent FROM       --4
+    (SELECT totals.day, ((errors.count / totals.sum) * 100) AS errorRate  --4
+    FROM
         (SELECT SUM(count), day FROM dailyStatus GROUP BY day)            --1
-            AS totals
+        AS totals
         INNER JOIN                                                        --3
         (SELECT count, day FROM dailyStatus WHERE status='404 NOT FOUND') --2
-            AS errors
+        AS errors
         ON totals.day = errors.day
     ) AS percents
-WHERE percents.percent > 0.01 ORDER BY day DESC;                          --5
+WHERE percents.errorRate > 1 ORDER BY day DESC;                           --5
 """
 # 1 Calculate the total number of requests made each day by
-#   summing together both response types (404 and 200)
+#   summing together requests with either response (type 404 and 200)
 # 2 Select out the number of requests that were errors made
 #   each day (type 404 only)
-# 3 Join the total number of requests made and number of
-#   requests that were errors using the day to match them up
+# 3 Join the total requests and error requests from each day
+#   into a single row
 # 4 Calculate the error rate by dividing each days error
-#   count by the total request count
+#   count by the total request count, then multiplying by
+#   100 to get a percent
 # 5 Only keep rows that have an error rate greater than 1%,
 #   and order them
 
 
-def TopArticles(conn, count):
+def TopArticles(count):
+    conn, cursor = connect_to_db('news')
+    cursor.execute(QUESTION1, {"limit": count})
+
     print("\nTOP " + str(count) + " ARTICLES")
-    q1 = QUESTION1.replace("{limit}", "LIMIT " + str(count))
-    cursor = conn.cursor()
-    cursor.execute(q1)
     for row in cursor:
         print('"' + row[2] + '" -- ' + str(row[0]) + ' views')
 
+    conn.close()
 
-def TopAuthors(conn):
-    print("\nTOP AUTHORS")
-    cursor = conn.cursor()
+
+def TopAuthors():
+    conn, cursor = connect_to_db('news')
     cursor.execute(QUESTION2)
+
+    print("\nTOP AUTHORS")
     for row in cursor:
         print('"' + str(row[1]) + '" -- ' + str(row[0]) + ' views')
 
+    conn.close()
 
-def HighErrorRates(conn):
-    print("\nHIGH ERROR RATES")
-    cursor = conn.cursor()
+
+def HighErrorRates():
+    conn, cursor = connect_to_db('news')
     cursor.execute(QUESTION3)
+
+    print("\nHIGH ERROR RATES")
     for row in cursor:
         print(str(row[0]) + ' -- ' + str(format(row[1], "0.2f")) + '% errors')
 
-
-def get_con():
-    return psycopg2.connect('dbname=news')
+    conn.close()
 
 
-conn = get_con()
+def connect_to_db(db_name):
+    try:
+        conn = psycopg2.connect('dbname=' + str(db_name))
+        cursor = conn.cursor()
+        return conn, cursor
+    except:
+        print("Error connecting to database...")
 
-print("Connected...")
-TopArticles(conn, 3)
-TopAuthors(conn)
-HighErrorRates(conn)
 
-
-"""
-cursor.execute(QUESTION2)
-print(cursor.fetchall())
-"""
+if __name__ == '__main__':
+    # Run the queries
+    print("Starting logs analysis...")
+    TopArticles(3)
+    TopAuthors()
+    HighErrorRates()
