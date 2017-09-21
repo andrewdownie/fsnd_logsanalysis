d0ce3c65c0bd746cdc326cb5fb9263ab47439cb1
Andrew Downie <andrew_dev@outlook.com>
2017-09-21 14:26:03 -0400

    Working on the readme.md

--- vagrant/README.md
+++ vagrant/README.md
@@ -1,14 +1,50 @@
-# Before running this program
-Two views will need to be created in the database before this program can be run. These views are: article_views and daily_status, and they are listed at the bottom of this readme. 
+# About this project
+This project requires vagrant, to setup vagrant see: <link to udacity vagrant setup instructions>
+
+TODO: 
+Describe what the purpose of the project is and what your program does. For example, you could mention that your Python script uses psycopg2 to query a mock PostgreSQL database for a fictional news website. You could discuss how the news database is structured, and what questions your script answers in the report it generates.
+
+# Project Setup
+## Setting up the vagrant environment
+### Download the vagrant environment file:
+https://github.com/udacity/fullstack-nanodegree-vm/blob/master/vagrant/Vagrantfile
+And navigate to where this file gets downloaded to.
+### Download and start the vagrant environment:
+```bash
+vagrant up
+```
+### Ssh into the vagrant environment:
+```bash
+vagrant ssh
+```
+
+## Setting up the database
+### Download the newsdata.zip file:
+https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip
+
+### Extract the newsdata.sql file: 
+Extract the newsdata.sql file into the vagrant folder from the newsdata.zip.
+
+From within the vagrant environment, run the command line command:
+```
+psql -d news -f newsdata.sql
+```
+
+## Creating the required views
+Two views will need to be created in the news database before this program can be run. These views are: article_views and daily_status, and they are listed at the bottom of this readme. 
 
 One method to create the required article_views and daily_status views is from within environment vagrant, enter the database interactively from the command line with: 
-`psql -d news`
+```sql
+psql -d news
+```
 
 and then copy and paste, one at a time, the sql at the bottom of this file to create the article_views and daily_status views.
 
 
-# To run this program, within the vagrant environment run the command:
-`python3 databaseAnalysis.py`
+# Running this project 
+```python
+python3 databaseAnalysis.py
+```
 
 
 # Program design:
@@ -18,19 +54,22 @@ The sql commands are at the top of the file, including the creation of views.
 # The views needed for this program are:
 
 ## article_views for questions 1 and 2
+```sql
 CREATE VIEW article_views AS
-SELECT COUNT(REPLACE(path, '/article/', '')),
+SELECT COUNT(path),
        articles.slug,
        articles.title,
        articles.author
     FROM articles
     INNER JOIN log
-    ON articles.slug
+    ON '/article/' || articles.slug = path
     LIKE REPLACE(path, '/article/', '')
     GROUP BY articles.slug, articles.id
     ORDER BY count DESC;
+```
 
 ## dailyStatus for question 3
+```sql
 CREATE VIEW daily_status AS
 SELECT COUNT(status),
           status,
@@ -38,6 +77,7 @@ SELECT COUNT(status),
    AS day
    FROM log
    GROUP BY day, status;
+```
 
 
 
--- vagrant/databaseAnalysis.py
+++ vagrant/databaseAnalysis.py
@@ -12,7 +12,7 @@ SELECT COUNT(path),
        articles.author
     FROM articles
     INNER JOIN log
-    ON articles.slug
+    ON '/article/' || articles.slug = path
     LIKE REPLACE(path, '/article/', '')
     GROUP BY articles.slug, articles.id
     ORDER BY count DESC;
@@ -21,13 +21,13 @@ SELECT COUNT(path),
 
 # Question 1: the top 3 articles
 QUESTION1 = """
-SELECT * FROM article_views ORDER BY count DESC LIMIT %(limit)s;
+SELECT title, count FROM article_views ORDER BY count DESC LIMIT %(limit)s;
 """
 
 
 # Question 2: authors ranked by article views
 QUESTION2 = """
-SELECT SUM(named_authors.count), named_authors.name FROM
+SELECT named_authors.name, SUM(named_authors.count) FROM
 (
     SELECT article_views.count, authors.name FROM article_views
     INNER JOIN authors
@@ -39,7 +39,7 @@ ORDER BY sum DESC;
 """
 
 
-# Question 3 requires view: dailyStatus
+# Question 3 requires view: daily_status
 """
 CREATE VIEW daily_status AS
 SELECT COUNT(status),
@@ -56,26 +56,29 @@ QUESTION3 = """
 SELECT * FROM
     (SELECT totals.day, ((errors.count / totals.sum) * 100) AS errorRate  --4
     FROM
-        (SELECT SUM(count), day FROM dailyStatus GROUP BY day)            --1
+        (SELECT SUM(count), day FROM daily_status GROUP BY day)           --1
         AS totals
         INNER JOIN                                                        --3
-        (SELECT count, day FROM dailyStatus WHERE status='404 NOT FOUND') --2
+        (SELECT count, day FROM daily_status WHERE status='404 NOT FOUND')--2
         AS errors
         ON totals.day = errors.day
     ) AS percents
 WHERE percents.errorRate > 1 ORDER BY day DESC;                           --5
 """
-# 1 Calculate the total number of requests made each day by
-#   summing together requests with either response (type 404 and 200)
-# 2 Select out the number of requests that were errors made
-#   each day (type 404 only)
-# 3 Join the total requests and error requests from each day
-#   into a single row
-# 4 Calculate the error rate by dividing each days error
-#   count by the total request count, then multiplying by
-#   100 to get a percent
-# 5 Only keep rows that have an error rate greater than 1%,
-#   and order them
+
+"""
+1 Calculate the total number of requests made each day by
+  summing together requests with either response (type 404 and 200)
+2 Select out the number of requests that were errors made
+  each day (type 404 only)
+3 Join the total requests and error requests from each day
+  into a single row
+4 Calculate the error rate by dividing each days error
+  count by the total request count, then multiplying by
+  100 to get a percent
+5 Only keep rows that have an error rate greater than 1%,
+  and order them
+"""
 
 
 def TopArticles(count):
@@ -84,7 +87,7 @@ def TopArticles(count):
 
     print("\nTOP " + str(count) + " ARTICLES")
     for row in cursor:
-        print('"' + row[2] + '" -- ' + str(row[0]) + ' views')
+        print('"{0}" -- {1} views'.format(row[0], row[1]))
 
     conn.close()
 
@@ -95,7 +98,7 @@ def TopAuthors():
 
     print("\nTOP AUTHORS")
     for row in cursor:
-        print('"' + str(row[1]) + '" -- ' + str(row[0]) + ' views')
+        print('"{0}" -- {1} views'.format(row[0], row[1]))
 
     conn.close()
 
@@ -106,7 +109,7 @@ def HighErrorRates():
 
     print("\nHIGH ERROR RATES")
     for row in cursor:
-        print(str(row[0]) + ' -- ' + str(format(row[1], "0.2f")) + '% errors')
+        print('{0} -- {1:.2f}% errors'.format(row[0], row[1]))
 
     conn.close()
 
